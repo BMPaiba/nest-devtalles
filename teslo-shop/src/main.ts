ee97102fecc757aa544738fe4c8bb654a5557c67
brian-paiba <brianmanuelpaiba@outlook.es>
2024-11-10 12:35:10 -0300

    configuracion de swagger

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RestAPI')
+    .setDescription('The Teslo API description')
+    .setVersion('1.0')
+    .addTag('teslo-shop')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
